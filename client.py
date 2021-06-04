2c138a1574c0fb866a97231f07e89fa6a11bc3dc
Cyrus Rice <cyrusric@amazon.com>
2021-06-04 08:56:29 +0000

    Final Commit

--- client.py
+++ client.py
@@ -9,13 +9,13 @@ message = { 'type' : '', 'data' : '', 'body' : ''}
 s = socket.socket()           
 # Define the port on which you want to connect 
 port = 12345                
-connected = False
+terminate = False
+connected = True
 
-def client_communications():
+def client_comm():
   instr = """Welcome to Cyrus's IRC Chat Client Application\n
         -----------------------------------------\n
         Below are the choices you can make\n
-        c = connect to server\n
         r = create a room\n
         l = list all rooms\n
         j = join a room\n
@@ -27,66 +27,84 @@ def client_communications():
   print(instr)
   
   while True:
-    choice = raw_input()
-    if choice == 'c':
-        # connect to the server on local computer 
-        s.connect(('0.0.0.0', port)) 
-        print("Socket succesfully connected to server")
-        connected = True
-        client_msg_sniffer_thread = Thread(target = client_msg_sniffer, name = 'client-msg-sniff-thread')
-        client_msg_sniffer_thread.start()
-    elif choice == 'r':
+    try:
+      choice = raw_input()
+      if terminate:
+          return
+      elif choice == 'r':
         # client creating a room
         message['type'] = 'CREATE_ROOM'
         message['data'] = raw_input("Enter a room name: ")
         s.send(json.dumps(message).encode('utf-8'))
-    elif choice == 'l':
+      elif choice == 'l':
         # list all rooms
         message['type'] = 'LIST_ROOMS'
         s.send(json.dumps(message).encode('utf-8'))
         print(s.recv(1024))
-    elif choice == 'j':
+      elif choice == 'j':
         # join a room
         message['type'] = 'JOIN_ROOM'
         message['data'] = raw_input("Enter a room name: ")
         s.send(json.dumps(message).encode('utf-8'))
-    elif choice == 'e':
+      elif choice == 'e':
         # leave a room
         message['type'] = 'LEAVE_ROOM'
         message['data'] = raw_input("Enter a room name: ")
         s.send(json.dumps(message).encode('utf-8'))
-    elif choice == 'm':
+      elif choice == 'm':
         # list members of a room
         message['type'] = 'LIST_MEMBERS'
         message['data'] = raw_input("Enter a room name: ")
         s.send(json.dumps(message).encode('utf-8'))
         print(s.recv(1024))
-    elif choice == 's':
+      elif choice == 's':
         # send message to a room
         message['type'] = 'SEND_MESSAGE'
         message['data'] = raw_input("Enter a room name: ")
         message['body'] = raw_input("Enter a message: ")
         s.send(json.dumps(message).encode('utf-8'))
-    elif choice == 'd':
+      elif choice == 'd':
         # disconnect from server
         message['type'] = 'DISCONNECT'
         #message['data'] = address?
         s.send(json.dumps(message).encode('utf-8'))
+        global connected
         connected = False
-        s.close()
-        print("Socket successfully disconnected from server")
-    elif choice == 'i':
+        #s.close()
+        #print("You've successfully disconnected from the IRC Server, exiting the Client application")
+        return
+      elif choice == 'i':
         print(instr)
-    else:
+      else:
         print("Invalid choice, enter 'i' to see the list of instructions again")
+    except (socket.error, socket.gaierror) as e:
+      print(e)
+      global connected
+      connected = False
+      return
   
-def client_msg_sniffer():
-  while connected:
-    print(s.recv(1024))
+def client_msg_sniff():
+  while True:
+      try:
+        msg = s.recv(1024)
+        print(msg)
+        if connected == False or len(msg) == 0:
+          s.close()
+          return
+      except (socket.error, socket.gaierror) as e:
+        print(e)
+        global terminate
+        terminate = True
+        return
   
 
 def main():
-  client_comm_thread = Thread(target = client_communications, name = 'client-comm-thread')
+  # connect to the server on local computer 
+  s.connect(('0.0.0.0', port)) 
+  print("You've succesfully connected to the IRC Server")
+  client_msg_sniff_thread = Thread(target = client_msg_sniff, name = 'client-msg-sniff-thread')
+  client_msg_sniff_thread.start()
+  client_comm_thread = Thread(target = client_comm, name = 'client-comm-thread')
   client_comm_thread.start()
 
 if __name__ == "__main__":
--- server.py
+++ server.py
@@ -6,78 +6,123 @@ from Queue import Queue
 
 rooms = {}
 client_inbox = {}
+client_terminated = {}
 message = {'type' : '', 'data' : ''}
 
 # function to create threads
 def client_connections():
-  s = socket.socket()         
-  print ("Socket successfully created")
-  port = 12345                
-  s.bind(('', port))         
-  print ("socket binded to %s" %(port)) 
-  s.listen(5)     
-  print ("socket is listening")            
+  try:
+    s = socket.socket()         
+    print ("Socket successfully created")
+    port = 12345                
+    s.bind(('', port))         
+    print ("socket binded to %s" %(port)) 
+    s.listen(5)     
+    print ("socket is listening")            
 
-  while True:
-    c, tmp_addr = s.accept()
-    addr = ''.join(str(tmp_addr))
-    print('Got connection from', tmp_addr)
-    # initialize each clients mailbox after creation
-    client_inbox[addr] = Queue()
-    client_comm_thread = Thread(target = client_communications, args = (c, tmp_addr), name='client-comm-thread')
-    client_comm_thread.start()
-    client_msg_sniffer_thread = Thread(target = client_msg_sniffer, args = (c, tmp_addr), name='client-msg-sniff-thread')
-    client_msg_sniffer_thread.start()
+    while True:
+      c, tmp_addr = s.accept()
+      addr = ''.join(str(tmp_addr))
+      print('Got connection from ', tmp_addr)
+      # initialize each clients mailbox after creation
+      client_inbox[addr] = Queue()
+      # initialize each clients terminated status to false
+      client_terminated[addr] = False
+      client_comm_thread = Thread(target = client_communications, args = (c, tmp_addr), name='client-comm-thread')
+      client_comm_thread.start()
+      client_msg_sniffer_thread = Thread(target = client_msg_sniffer, args = (c, tmp_addr), name='client-msg-sniff-thread')
+      client_msg_sniffer_thread.start()
+  except (socket.error, socket.gaierror, ValueError) as e:
+    print(e)
+    return
+
 
 def client_msg_sniffer(c, tmp_addr):
   addr = ''.join(str(tmp_addr))
-  while True:
+  while client_terminated[addr] == False:
     msg = client_inbox[addr].get()
-    c.send(msg)
+    try:
+      c.send(msg)
+    except (socket.error, socket.gaierror, ValueError) as e:
+      print(e)
+      client_terminated[addr] = True
+      return
 
 def client_communications(c, tmp_addr):
-  addr = ''.join(str(tmp_addr))
-  client_msg = json.loads(c.recv(1024))
-  while client_msg['type'] != 'DISCONNECT':
-    if client_msg['type'] == 'CREATE_ROOM':
-      # Create a new room
-      rooms[client_msg['data']] = []
-      print("room " + client_msg['data'] + " created!")
-    elif client_msg['type'] == 'LIST_ROOMS':
-      # Return list of all rooms
-      rooms_list = 'List of rooms\n---------------\n'
-      for key in rooms:
-        rooms_list += key + '\n'
-      c.send(rooms_list)
-    elif client_msg['type'] == 'JOIN_ROOM':
-      # add client user/addr to room
-      rooms[client_msg['data']].append(addr)
-      print("Client " + addr + "successfully joined room " + client_msg['data'])
-      # initialize the clients mailbox for the room they just joined
-      #client_inbox[addr][client_msg['data']] = []
-    elif client_msg['type'] == 'LEAVE_ROOM':
-      rooms[client_msg['data']].remove(addr)
-      print("Client " + addr + "successfully left room " + client_msg['data'])
-      # clear clients mailbox for the room they just left
-      #client_inbox[addr][client_msg['data']] = []
-    elif client_msg['type'] == 'LIST_MEMBERS':
-      room = client_msg['data']
-      members_list = 'Members of room ' + room + '\n---------------------\n'
-      for client_addr in rooms[room]:
-        members_list += client_addr + '\n'
-      c.send(members_list)
-    elif client_msg['type'] == 'SEND_MESSAGE':
-      room = client_msg['data']
-      for client_addr in rooms[room]:
-        client_inbox[client_addr].put('[' + room + '] ' + addr + ': ' + client_msg['body'])
+  try:
+    addr = ''.join(str(tmp_addr))
     client_msg = json.loads(c.recv(1024))
+    while (client_msg['type'] != 'DISCONNECT') and (client_terminated[addr] == False):
+      if client_msg['type'] == 'CREATE_ROOM':
+        if client_msg['data'] not in rooms:
+          # Create a new room
+          rooms[client_msg['data']] = []
+          print("room " + client_msg['data'] + " created!")
+          c.send("room " + client_msg['data'] + " created!")
+        else:
+          # Room already exists
+          c.send("Failed to create room \"" + client_msg['data'] + "\", that room already exists\n")
+      elif client_msg['type'] == 'LIST_ROOMS':
+        # Return list of all rooms
+        rooms_list = 'List of rooms\n---------------\n'
+        for key in rooms:
+          rooms_list += key + '\n'
+        c.send(rooms_list)
+      elif client_msg['type'] == 'JOIN_ROOM':
+        # Room doesn't exist
+        if client_msg['data'] not in rooms:
+          c.send("Failed to join room \"" + client_msg['data'] + "\", that room doesn't exist\n") 
+        # Client already in room
+        elif addr in rooms[client_msg['data']]:
+          c.send("Failed to join room \"" + client_msg['data'] + "\", you're already in that room!\n") 
+        else:
+          # add client user/addr to room
+          rooms[client_msg['data']].append(addr)
+          print("Client " + addr + "successfully joined room " + client_msg['data'])
+          c.send("You've successfully joined room " + client_msg['data'])
+      elif client_msg['type'] == 'LEAVE_ROOM':
+        # Room doesn't exist
+        if client_msg['data'] not in rooms:
+          c.send("Failed to leave room \"" + client_msg['data'] + "\", that room doesn't exist\n") 
+        # Client not in room
+        elif addr not in rooms[client_msg['data']]:
+          c.send("Failed to leave room \"" + client_msg['data'] + "\", you weren't in that room!\n") 
+        else:
+          rooms[client_msg['data']].remove(addr)
+          print("Client " + addr + "successfully left room " + client_msg['data'])
+          c.send("You've successfully left room " + client_msg['data'])
+      elif client_msg['type'] == 'LIST_MEMBERS':
+        # Room doesn't exist
+        if client_msg['data'] not in rooms:
+          c.send("Failed to list members of room \"" + client_msg['data'] + "\", that room doesn't exist\n") 
+        else:
+          room = client_msg['data']
+          members_list = 'Members of room ' + room + '\n---------------------\n'
+          for client_addr in rooms[room]:
+            members_list += client_addr + '\n'
+          c.send(members_list)
+      elif client_msg['type'] == 'SEND_MESSAGE':
+        # Room doesn't exist
+        if client_msg['data'] not in rooms:
+          c.send("Failed to send message to room \"" + client_msg['data'] + "\", that room doesn't exist\n") 
+        # Client not in room
+        elif addr not in rooms[client_msg['data']]:
+          c.send("Failed to send message to room \"" + client_msg['data'] + "\", you weren't in that room!\n") 
+        else:
+          room = client_msg['data']
+          for client_addr in rooms[room]:
+            client_inbox[client_addr].put('[' + room + '] ' + addr + ': ' + client_msg['body'])
+      client_msg = json.loads(c.recv(1024))
 
-  # send a thank you message to the client. 
-  #c.send("thank you!") 
-  print("client " + addr + " closed connection")
+    print("client " + addr + " closed connection")
+    c.send("You've successfully closed connection from you to the server, exiting Client IRC Application")
 
-  # Close the connection with the client 
-  c.close() 
+    # Close the connection with the client 
+    c.close() 
+  except (socket.error, socket.gaierror, ValueError) as e:
+    print(e)
+    client_terminated[addr] = True
+    return
 
 def main():
   connection_thread = Thread(target = client_connections, name='connection-thread')
